3a777e7d90a8a14a2c1cbbfb045de70b7d5c6e82
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-01 09:01:18 +0200

    Implement same array objects for comperison

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 const GRID_HEIGHT = 8;
 const GRID_LENGTH = 8;
 
+let grid = [];
+for (let m = 0; m < GRID_HEIGHT; m++) {
+    grid.push([])
+    for (let n = 0; n < GRID_LENGTH; n++) grid[m].push([n, m])
+}
+
 function knightMoves (start, end) {
-    let q = [[start, []]];
+    let q = [[grid[start[1]][start[0]], []]];
     let done = new Set();
     let res;
     while (q.length) {
         const curr = q.pop();
-        const pos = curr[0];
+        if (curr[0][0] < 0 || curr[0][0] > GRID_LENGTH - 1 || curr[0][1] < 0 || curr[0][1] > GRID_HEIGHT - 1) continue
+        const pos = grid[curr[0][1]][curr[0][0]];
         const list = curr[1];
-        if (done.has(curr) || curr[0] < 0 || curr[0] > GRID_LENGTH - 1 || curr[1] < 0 || curr[1] > GRID_HEIGHT - 1) continue
-        if (pos == end) {
+        console.log(pos);
+        if (done.has(pos)) continue
+        if (pos == grid[end[1]][end[0]]) {
             res = [...list, pos];
             break
         }
